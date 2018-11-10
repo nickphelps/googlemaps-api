66d506861f04e114f25c299d06f9bab8a5bb7499
KimjManansala <kimjmanansala@gmail.com>
2018-11-10 13:11:29 -0600

    Fixed the data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,27 @@
-let Perth = {lat: 31.9505, lng: 115.8605}
-let Brisbane = {lat: 27.4698, lng 153.0251}
-let Darwin = {lat: 12.4634, lng 130.8456}
-let Adelaide = {lat: 34.9285, lng 138.6007}
-let Melbourne = {lat: 37.8136, lng 144.9631}
-let Sydney = {lat: 33.8688, lng 151.2093}
+let cities = [
+    Perth = {
+        lat: 31.9505, 
+        lng: 115.8605
+    },
+    Brisbane = {
+        lat: 27.4698, 
+        lng: 153.0251
+    },
+    Darwin = {
+        lat: 12.4634, 
+        lng: 130.8456
+    },
+    Adelaide = {
+        lat: 34.9285, 
+        lng: 138.6007
+    },
+    Melbourne = {
+        lat: 37.8136, 
+        lng: 144.9631
+    },
+    Sydney = {
+        lat: 33.8688, 
+        lng: 151.2093
+    }
+
+]
